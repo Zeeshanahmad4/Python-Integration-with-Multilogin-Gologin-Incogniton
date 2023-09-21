dccd6357feb929cf4e8e649ef7749eea03fefb42
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-21 18:19:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
- 
+# API endpoints
+MULTILOGIN_API_ENDPOINT = "https://api.multilogin.com"
+GOLOGIN_API_ENDPOINT = "https://api.gologin.com"
+INCOGNITON_API_ENDPOINT = "https://api.incogniton.com"
+
+# Other configuration details can be added as the application requirements grow. 
