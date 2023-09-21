8afc05d152fc67a0a3514a39a10c23f679e99700
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-21 18:11:51 +0500

    Update encryption.py

--- data_storage/encryption.py
+++ data_storage/encryption.py
@@ -1,1 +1,11 @@
+from base64 import b64encode, b64decode
+
+class EncryptionUtil:
+    def encrypt(self, data):
+        """Encrypts the given data."""
+        return b64encode(data.encode()).decode()
+    
+    def decrypt(self, encrypted_data):
+        """Decrypts the given encrypted data."""
+        return b64decode(encrypted_data.encode()).decode()
  
