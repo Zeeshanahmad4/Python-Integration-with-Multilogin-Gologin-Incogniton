c34e2112553026aee7d2d8f9fbc58eff9810ce74
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-21 18:14:22 +0500

    Update logging_util.py

--- utils/logging_util.py
+++ utils/logging_util.py
@@ -1,1 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def setup_logger():
+    """Set up the logger for the application."""
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logger.setLevel(logging.DEBUG)
  
