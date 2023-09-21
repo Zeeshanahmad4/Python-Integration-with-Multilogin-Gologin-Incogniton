c7e0038b36719f8701bbe4e7d150222642dbb5c1
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-21 18:11:10 +0500

    Update database_connector.py

--- data_storage/database_connector.py
+++ data_storage/database_connector.py
@@ -1,1 +1,17 @@
- 
+import sqlite3
+
+class DatabaseConnector:
+    def __init__(self, db_config):
+        self.db_config = db_config
+        self.connection = None
+    
+    def connect(self):
+        """Connect to the database."""
+        self.connection = sqlite3.connect(self.db_config['db_name'])
+    
+    def close(self):
+        """Close the database connection."""
+        if self.connection:
+            self.connection.close()
+
+    # Other database related methods can be added as required 
